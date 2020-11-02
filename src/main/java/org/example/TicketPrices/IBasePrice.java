09d63901f062cb2c9c41d427203811eb79111259
Dainis <dainisp@inbox.lv>
2020-11-02 07:04:04 +0200

    Some reorganization...

--- src/main/java/org/example/TicketPrices/IBasePrice.java
+++
@@ -1,9 +0,0 @@
-package org.example.TicketPrices;
-
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface IBasePrice {
-    float getBasePrice(String busTerminalName);
-}
--- src/main/java/org/example/TicketPrices/ITicketPrices.java
+++
@@ -1,8 +0,0 @@
-package org.example.TicketPrices;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ITicketPrices {
-    TicketPricesInfo getTicketPrices(BagInfo bagInfo);
-}
